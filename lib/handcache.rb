7d6471dd59b204ad0bb8d2df188038e2df335750
Wen-Tien Chang <ihower@gmail.com>
2009-08-19 03:58:38 +0800

    Use moneta to unifiy key-value store interface

--- lib/handcache.rb
+++ lib/handcache.rb
@@ -1,34 +1,34 @@
 module Handcache
   
   def self.get(id)
-    return CacheStore.read(id)
+    return CacheStore[id]
   end
   
   def self.compressed_get(id)
-    cache_data = CacheStore.read(id)
+    cache_data = CacheStore[id]
     return nil unless cache_data
     return Marshal.load(ActiveSupport::Gzip.decompress(cache_data))
   end
     
   def self.get_and_set(id, options = {}, &block)
-    cache_data = CacheStore.read(id)    
+    cache_data = CacheStore[id]
     if cache_data
       return cache_data
     else
       data = block.call
-      CacheStore.write(id, data, options)
+      CacheStore.store(id, data, options)
       return data
     end
   end
   
   def self.compressed_get_and_set(id, options = {}, &block)
-    cache_data = CacheStore.read(id)    
+    cache_data = CacheStore[id]
     if cache_data
       return Marshal.load(ActiveSupport::Gzip.decompress(cache_data))
     else
       value = block.call
       data = ActiveSupport::Gzip.compress(Marshal.dump(value))
-      CacheStore.write(id, data, options)
+      CacheStore.store(id, data, options)
       return value
     end
     
